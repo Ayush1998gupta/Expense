005bf194ea740794391f0777972d8982b2bea1d0
Ayush <aayush199813@gmail.com>
2022-08-06 21:18:09 +0530

    Adding conditional return

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredyear, setFilteredYear] = useState('2020');
@@ -15,25 +15,15 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredyear;
   });
 
-  let expenseContent = <p>No Exoense Found.</p>;
-
-  if (filteredExpress.length > 0) {
-    expenseContent = filteredExpress.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      ></ExpenseItem>
-    ));
-  }
+  
+  
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredyear}
         onYearChange={onYearChangeHandler}
       />
-      {expenseContent}
+      <ExpensesList items={filteredExpress} />
     </Card>
   );
 };
